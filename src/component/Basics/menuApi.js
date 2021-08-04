84dc24a4b7f1bb7bdb68ef677ae794b208e55c4f
BHAWANI SHANKAR BEHERA <bhawanishbehera05@gmail.com>
2021-08-05 01:15:12 +0530

    my first commit react website

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,113 @@
+    
+const Menu = [
+    {
+      id: 1,
+      image: "image/breakfast03.jpg",
+      name: "Bread",
+      category: "breakfast",
+      price: "20₹",
+      description:
+        " A slice of bread is certainly acceptable to have with breakfast; the more complex the carbohydrate the better too. Just realize that with most things moderation is key. You have an entire day to make use of that energy provided by the carbs in that whole grain slice of toast; what better way to start your morning.",
+    },
+  
+    {
+      id: 2,
+      image: "image/breakfast01.jpg",
+      name: "Fried Egg",
+      category: "breakfast",
+      price: "30₹",
+      description:
+"A fried egg is a cooked dish made from one or more eggs which are removed from their shells and placed into a frying pan and fried with minimal accompaniment."    },
+    {
+      id: 3,
+      image: "image/breakfast02.jpg",
+      name: "Alu da Paratha",
+      category: "breakfast",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../image/lunch01.jpg",
+      name: "Full plate Special Non-Veg",
+      category: "lunch",
+      price: "120₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../image/lunch02.jpg",
+      name: "Veg Special Plate",
+      category: "lunch",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../image/lunch03.jpg",
+      name: "All India Thali",
+      category: "lunch",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../image/evening01.jpg",
+      name: "Chicken Biriyani",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../image/evening02.jpg",
+      name: "Egg Roll",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../image/evening03.jpg",
+      name: "Alu Chop",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "../image/dinner01.jpg",
+      name: "Dosa",
+      category: "dinner",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 11,
+      image: "../image/dinner02.jpg",
+      name: "Roti Special",
+      category: "dinner",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      image: "../image/dinner03.jpg",
+      name: "South Thali",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
